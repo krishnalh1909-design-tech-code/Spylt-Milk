76831ca637913a1a8219ed65de826cdb4be89ca4
krishnalh1909-design-tech-code <krishnalh1909@gmail.com>
2025-09-23 00:11:32 +0530

    landing page responsive

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ setTimeout(() => {
   document.querySelector(".bgVid video").play();
 }, 4000);
 
- 
 gsap.from(".page1 .title h1",{
   y:500,
   duration:0.5,
@@ -30,12 +29,36 @@ gsap.from(".page1 .title h1",{
 },)
 
 gsap.from(".title2",{
-  // width:"0",
   x:100,
   opacity:0,
   duration:0.5,
-  // stagger:0.05
   delay:5
 })
 
-// // npx @tailwindcss/cli -i ./input.css -o ./output.css --watch
+// // // npx @tailwindcss/cli -i ./input.css -o ./output.css --watch
+
+let wasMobile = window.innerWidth < 768;
+
+function toggleVisibility() {
+  const bgVid = document.querySelector(".bgVid");
+  if (!bgVid) return;
+
+  const isMobile = window.innerWidth < 768;
+
+  if (isMobile) {
+    bgVid.style.display = "none";
+  } else {
+    bgVid.style.display = "block";
+  }
+
+  // Reload only when switching from mobile to desktop
+  if (wasMobile && !isMobile) {
+    location.reload();
+  }
+
+  wasMobile = isMobile;
+}
+
+window.addEventListener("load", toggleVisibility);
+window.addEventListener("resize", toggleVisibility);
+
